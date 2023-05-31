5874dc3c993fae92ee39fb7a1bd6e73dff33f1fa
unknown <icw.ekta@gmail.com>
2023-05-31 17:01:00 +0530

    Update Page (PC)

---
+++ immigrationchambers.com.au/html/assets/js/main.js
@@ -0,0 +1,27 @@
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function($) {
+
+   $('[data-bs-toggle="tooltip"]').tooltip();
+
+   $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+      $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+      e.preventDefault();
+   });
+
+   if ($('.main-header').length) {
+      if (jQuery(this).scrollTop() > 100) {
+         $(".main-header").addClass("fixed-header");
+      } else {
+         $(".main-header").removeClass("fixed-header");
+      }
+
+      $(window).scroll(function () {
+         if (jQuery(this).scrollTop() > 100) {
+            $(".main-header").addClass("fixed-header");
+         } else {
+            $(".main-header").removeClass("fixed-header");
+         }
+      });
+   }
+})
