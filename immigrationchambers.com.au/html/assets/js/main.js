1370fe39c776a733717ab5da2dc66b6dbf76891c
unknown <icw.ekta@gmail.com>
2023-06-02 19:02:39 +0530

    Update Page (PC)

--- immigrationchambers.com.au/html/assets/js/main.js
+++ immigrationchambers.com.au/html/assets/js/main.js
@@ -2,26 +2,46 @@ var $ = jQuery.noConflict();
 
 jQuery(document).ready(function($) {
 
-   $('[data-bs-toggle="tooltip"]').tooltip();
+    $('[data-bs-toggle="tooltip"]').tooltip();
 
-   $('.toggle-sidebar,.bg-overly').on('click', function (e) {
-      $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
-      e.preventDefault();
-   });
+    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+        $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+        e.preventDefault();
+    });
 
-   if ($('.main-header').length) {
-      if (jQuery(this).scrollTop() > 100) {
-         $(".main-header").addClass("fixed-header");
-      } else {
-         $(".main-header").removeClass("fixed-header");
-      }
-
-      $(window).scroll(function () {
-         if (jQuery(this).scrollTop() > 100) {
+    if ($('.main-header').length) {
+        if (jQuery(this).scrollTop() > 100) {
             $(".main-header").addClass("fixed-header");
-         } else {
+        } else {
             $(".main-header").removeClass("fixed-header");
-         }
-      });
-   }
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 100) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+    }
+    if ($('.testimonial-slider').length) {
+        $('.testimonial-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            dots: true,
+            slidesToShow: 2,
+            infinite: true,
+            accessibility: false,
+            responsive: [
+                {
+                breakpoint: 992,
+                    settings: {
+                        arrows: false,
+                        dots:true,
+                    }
+                }
+            ]
+        });
+    };
 })
