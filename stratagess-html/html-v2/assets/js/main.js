bbdea1fa312d81090b9d6d6bedb1e254f0fbf99d
unknown <icw.ekta@gmail.com>
2023-07-18 19:16:11 +0530

    Update Page (PC)

---
+++ stratagess-html/html-v2/assets/js/main.js
@@ -0,0 +1,80 @@
+jQuery(document).ready(function($) {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+ 
+    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+       $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+       e.preventDefault();
+    });
+ 
+    if ($('.main-header').length) {
+       if (jQuery(this).scrollTop() > 100) {
+          $(".main-header").addClass("fixed-header");
+       } else {
+          $(".main-header").removeClass("fixed-header");
+       }
+ 
+       $(window).scroll(function () {
+          if (jQuery(this).scrollTop() > 100) {
+             $(".main-header").addClass("fixed-header");
+          } else {
+             $(".main-header").removeClass("fixed-header");
+          }
+       });
+    }
+
+   $(".counter").each(function () {
+      var $this = $(this),
+         countTo = $this.attr("data-countto");
+         countDuration = parseInt($this.attr("data-duration"));
+      $({ counter: $this.find('span').text() }).animate({
+         counter: countTo
+      },
+      {
+         duration: countDuration,
+         easing: "linear",
+         step: function () {
+            $this.find('span').text(Math.floor(this.counter));
+         },
+         complete: function () {
+            $this.find('span').text(this.counter);
+         }
+      });
+   });
+   $('.categories-btn').on('click', function(){
+      $(this).toggleClass('active');
+   })
+})
+
+
+$('.menu-item-has-children .arrow').on('click',function(event){
+   event.preventDefault();
+   $(this).toggleClass('is-active');
+   // $(this).next().toggleClass('d-none');
+   $(this).parent().find('.sub-menu').first().toggle(300);
+   $(this).parent().siblings().find('.sub-menu').hide(200);
+});
+
+if ($('.logo-slider').length) {
+   var logoslider = new Splide( '.logo-slider', {
+      type   : 'loop',
+      pagination : false,
+      autoWidth: true,
+      arrows: false,      
+   });
+   
+   logoslider.mount();
+   };
+ 
+
+    /* collapse */
+    $(".collapse-item .collapse-title").click(function () {
+      if ($(this).closest(".collapse-item").hasClass("is-open")) {
+         $(this).closest(".collapse-item").stop().removeClass("is-open");
+         $(this).closest(".collapse-item").find(".collapse-body").stop().slideUp();
+      } else {
+         $(".collapse-item").removeClass("is-open");
+         $(".collapse-item").find(".collapse-body").stop().slideUp();
+         $(this).closest(".collapse-item").stop().addClass("is-open");
+         $(this).closest(".collapse-item").find(".collapse-body").stop().slideDown();
+      }
+  });
