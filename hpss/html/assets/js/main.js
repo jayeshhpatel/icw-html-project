5ae137927aa8ad33c0916c24e294043f5006d1ec
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2023-08-21 19:55:58 +0530

    Update Page (PC)

---
+++ hpss/html/assets/js/main.js
@@ -0,0 +1,51 @@
+/* Moblie Menu */
+var $ = jQuery.noConflict();
+jQuery(document).ready(function($) {
+
+    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+        $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+        e.preventDefault();
+    });
+    $(window).resize(function() {
+        var windowWidth = $(window).width();
+        if(windowWidth > 991){
+            $('.bg-overly,.toggle-sidebar,body,.main-header').removeClass('is-visible');
+        }
+    });
+
+    if ($('.main-header').length) {
+        if (jQuery(this).scrollTop() > 100) {
+            $(".main-header").addClass("fixed-header");
+        } else {
+            $(".main-header").removeClass("fixed-header");
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 100) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+    }
+
+    if ($('li.menu-item-has-children').length) {
+        $("li.menu-item-has-children > a").after('<i class="arrow"></i>');
+    }
+    $('li.menu-item-has-children .arrow').on('click',function(event){
+        event.preventDefault();
+        $(this).toggleClass('is-active');
+        $(this).parent().siblings().find('.sub-menu').slideUp();
+        $(this).parent().find('.sub-menu').first().toggle(300);
+        
+        //Hide menu when clicked outside
+        $(this).parent().find('.sub-menu').parent().mouseleave(function(){ 
+           var thisUI = $(this);
+           $('html').click(function(){
+              thisUI.children(".sub-menu").hide();
+              thisUI.children(".arrow").removeClass('is-active');
+              $('html').unbind('click');
+           });
+        });
+    });
+});
