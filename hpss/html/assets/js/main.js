8e52f1c4a1f6ad7f63ae06c197616c10f2978483
unknown <icw.ekta@gmail.com>
2023-08-29 21:14:35 +0530

    Update Page (PC)

--- hpss/html/assets/js/main.js
+++ hpss/html/assets/js/main.js
@@ -53,45 +53,53 @@ jQuery(document).ready(function($) {
         }
 });
 
-// if ($('.event-slider').length) {
-//     var eventSlider = new Splide( '.event-slider', {
-//         perPage: 2,
-//         perMove: 1,
-//         arrowPath: 'M11 2L28.949 19.9998L11 38',        
-//         pagination : false,
-//         gap    : '30px',
-//         padding: '1rem',
-//         snap   : true,
-//         type   : 'loop',
-//         breakpoints: {
-//             767: {
-//                 perPage: 1,
-//                 arrows: false,
-//                 pagination: true,
-//             },
-//         },
-//     });  
-//     eventSlider.mount();
-//  };
-
- 
-
-
-
-
-
-
 if ($('.event-slider').length) {
+    var eventSlider = new Splide( '.event-slider', {
+        perPage: 2,
+        perMove: 1,
+        arrowPath: 'M11 2L28.949 19.9998L11 38',        
+        pagination : false,
+        gap    : '30px',
+        padding: '1rem',
+        snap   : true,
+        type   : 'loop',
+        breakpoints: {
+            767: {
+                perPage: 1,
+                arrows: false,
+                pagination: true,
+            },
+        },
+    });  
+    eventSlider.mount();
+ };
 
-var eventSlider = new Splide( '.splide', {
-    type   : 'loop',
-    perPage: 2,
-    perMove: 1,
-    arrowPath: 'M11 2L28.949 19.9998L11 38',
-    pagination : false,
-    gap: '30px',
-    snap   : true,
-});
-  
-  eventSlider.mount();
+ /*
+    Masonry Layout JS Vanilla
+    https://github.com/Mathiew82/masonry-layout-vanilla
+*/
+
+var fecthMasonry = function (container, items, columns) {
+    var CONTAINER_EL = document.querySelector("." + container);
+    var WRAPPER_CONTAINER_EL = CONTAINER_EL.parentNode;
+    var ITEMS_ELS = document.querySelectorAll("." + items);
+    CONTAINER_EL.parentNode.removeChild(CONTAINER_EL);
+    var NEW_CONTAINER_EL = document.createElement('div');
+    NEW_CONTAINER_EL.setAttribute('id', container);
+    NEW_CONTAINER_EL.classList.add('masonry-layout', "columns-" + columns);
+    WRAPPER_CONTAINER_EL.appendChild(NEW_CONTAINER_EL);
+    for (var i = 1; i <= columns; i++) {
+        var COLUMN = document.createElement('div');
+        COLUMN.classList.add("masonry-column-" + i);
+        NEW_CONTAINER_EL.appendChild(COLUMN);
+    }
+    var countColumn = 1;
+    ITEMS_ELS.forEach(function (item) {
+        var col = document.querySelector("#" + container + " > .masonry-column-" + countColumn);
+        col.appendChild(item);
+        countColumn = countColumn < columns ? countColumn + 1 : 1;
+    });
+};
+if ($('.masonry-grid-layout').length) {
+    fecthMasonry('masonry-grid-layout', 'card', 2);
 }
