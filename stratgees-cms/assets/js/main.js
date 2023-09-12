9f7f0b87d231fae53b7429d0c27de26261a1faff
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2023-09-12 14:45:48 +0530

    Update HTML in stratgees-cms (PC)

--- stratgees-cms/assets/js/main.js
+++ stratgees-cms/assets/js/main.js
@@ -69,3 +69,22 @@ if ($('.logo-slider').length) {
 //       $('.testimonail-card .sort-info').css('height', info_height+'px');
 //    }, 1000);
 // }
+
+$(".result-progress").each(function(){
+  
+   var $bar = $(this).find(".bar");
+   var $val = $(this).find("span");
+   var perc = parseInt( $val.text(), 10);
+ 
+   $({p:0}).animate({p:perc}, {
+     duration: 3000,
+     easing: "swing",
+     step: function(p) {
+       $bar.css({
+         transform: "rotate("+ (45+(p*1.8)) +"deg)", // 100%=180° so: ° = % * 1.8
+         // 45 is to add the needed rotation to have the green borders at the bottom
+       });
+       $val.text(p|0);
+     }
+   });
+ });
