6c548afc34def8df7453a688880fafa3b4616af3
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2023-09-15 15:50:47 +0530

    Update Home Page in gentium-global-migration(PC)

---
+++ gentium-global-migration/html/assets/js/main.js
@@ -0,0 +1,49 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function($) {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+        $('.bg-overly,.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+        e.preventDefault();
+    });
+
+    if ($('.main-header').length) {
+        if (jQuery(this).scrollTop() > 100) {
+            $(".main-header").addClass("fixed-header");
+        } else {
+            $(".main-header").removeClass("fixed-header");
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 100) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+    }
+
+    if ($('li.menu-item-has-children').length) {
+        $("li.menu-item-has-children > a").after('<i class="arrow"></i>');
+    }
+    $('li.menu-item-has-children .arrow').on('click',function(event){
+        event.preventDefault();
+        $(this).toggleClass('is-active');
+        $(this).parent().find('.sub-menu').first().toggle(300);
+        $(this).parent().siblings().find('.sub-menu').hide(200);
+        
+        //Hide menu when clicked outside
+        $(this).parent().find('.sub-menu').parent().mouseleave(function(){ 
+            var thisUI = $(this);
+            $('html').click(function(){
+                thisUI.children(".sub-menu").hide();
+                thisUI.children(".arrow").removeClass('is-active');
+                $('html').unbind('click');
+            });
+        });
+    });
+});
