061cb01b1cd82b5c4745b3b74cf6757337d3686b
unknown <icw.ekta@gmail.com>
2023-11-01 19:19:50 +0530

    Update New Pages i tubo.ph HTML (PC)

--- tubo-html/html/assets/js/main.js
+++ tubo-html/html/assets/js/main.js
@@ -22,4 +22,22 @@ jQuery(document).ready(function($) {
             e.stopPropagation();
         }
     });
+    
+
 })
+$('.toggleBtn').on('click', function(e) {
+    if($(this).hasClass('btnView')){
+        $(this).parents('.card').find('.input-box-block').slideUp();
+        $(this).parents('.card').find('.result-box').slideDown();
+        $(this).html('Add new <span class="btn-icon"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 16 16" fill="none"><path d="M7.9987 3.33337V12.6667M3.33203 8.00004H12.6654" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></span>');
+        $(this).removeClass('btnView').addClass('addNew');
+    } else if($(this).hasClass('addNew')){
+        $(this).parents('.card').find('.input-box-block').slideDown();
+        $(this).parents('.card').find('.result-box').slideUp();
+        $(this).html('View');
+        $(this).removeClass('addNew').addClass('btnView');
+    }
+});
+$('.btn-view').on('click', function(e) {
+   
+});
