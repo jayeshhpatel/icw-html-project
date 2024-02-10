9fa676d6d21b17a384333a2d752a3a5d49efdee3
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2024-02-10 16:44:47 +0530

    Update Page (PC)

--- dani/deepchecks.com/html-theme/assets/js/main.js
+++ dani/deepchecks.com/html-theme/assets/js/main.js
@@ -384,7 +384,7 @@ jQuery(document).ready(function($) {
             slidesToShow: 3,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 3000,
+            autoplaySpeed: 2000,
             centerMode: true,
         });
     }
