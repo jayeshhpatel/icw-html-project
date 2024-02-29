6cc272f92e8551b9956de8b1ff05f5ed46b768bc
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2024-02-29 12:42:46 +0530

    Update Slider Section(PC)

--- pvml-html/theme-html/assets/js/main.js
+++ pvml-html/theme-html/assets/js/main.js
@@ -304,3 +304,51 @@ function hubspot_labels() {
         $(this).removeClass('is-active');
     });
 })(jQuery, window, document);
+
+function splideSlider(slideID) {
+    var splide_workspace_title_img = new Splide('#' + slideID, {
+        arrows: false,
+        rewind: true,
+        gap: 20,
+        snap: true,
+        type: 'fade',
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+    // Listen to the 'mounted' event to get the slider instance
+    splide_workspace_title_img.on('mounted', function () {
+        // Get all video elements within the slider
+        var fullvideos = document.querySelectorAll('#'+slideID+'video');
+
+        // Listen to the 'moved' event to play/pause fullvideos on slide change
+        splide_workspace_title_img.on('moved', function (newIndex) {
+            var currentSlideNumber = newIndex + 1;
+            // Select all elements with the class 'splide_current'
+            var elementsWithSplideCurrent = document.querySelectorAll('.splide_current');
+            // Update the content of each selected element
+            elementsWithSplideCurrent.forEach(function (element) {
+                if (element) {
+                    element.textContent = formatSlideNumber(currentSlideNumber);
+                }
+            });
+
+            // Pause all fullvideos
+            if (fullvideos && fullvideos.length > 0) {
+                fullvideos.forEach(function (video, index) {
+                    if (index !== newIndex) {
+                        video.pause();
+                        video.currentTime = 0;
+                    }
+                });
+                // Play the video in the current slide
+                fullvideos[newIndex].play();
+            }
+        });
+    });
+    splide_workspace_title_img.mount();
+    
+    // Format slide number with leading zeros
+    function formatSlideNumber(number) {
+        return number < 10 ? '0' + number : number.toString();
+    }
+}
