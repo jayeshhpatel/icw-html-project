110045d154a9cd6558fbfd8e61af3adec0dfbdce
unknown <icw.ekta@gmail.com>
2024-08-14 20:08:52 +0530

    Update header in home page for ecolightdiamond HTML (PC)

---
+++ ecolightdiamond/html/assets/js/main.js
@@ -0,0 +1,36 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function($) {
+
+    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+        $('.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+        e.preventDefault();
+    });
+
+    if ($('.main-header').length) {
+        if (jQuery(this).scrollTop() > 100) {
+            $(".main-header").addClass("fixed-header");
+        } else {
+            $(".main-header").removeClass("fixed-header");
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 100) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+    }
+    if ($('li.menu-item-has-children').length) {
+        $("li.menu-item-has-children > a").after('<i class="arrow"></i>');
+    }
+    $('li.menu-item-has-children .arrow').on('click',function(event){
+        event.preventDefault();
+        $(this).toggleClass('is-active');
+        $(this).parent().find('.sub-menu').first().toggle(300);
+    });
+});
