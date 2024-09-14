7d40fbd7ef5600389389245ea8e3ea45ab46ce41
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2024-09-14 17:41:58 +0530

    Update header, footer & Page header Section in northerncornsoyexpo (PC)

--- northerncornsoyexpo-html/html/assets/js/main.js
+++ northerncornsoyexpo-html/html/assets/js/main.js
@@ -7,12 +7,7 @@ jQuery(document).ready(function($) {
     if ($('[data-bs-toggle=tooltip]').length) {
         $('body').tooltip({ selector: '[data-bs-toggle=tooltip]' });
     }
-
-    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
-        $('.toggle-sidebar,body,.main-header').toggleClass('is-visible');
-        e.preventDefault();
-    });
-
+    
     if ($('.main-header').length) {
         if (jQuery(this).scrollTop() > 100) {
             $('.main-header').addClass('fixed-header');
@@ -37,46 +32,7 @@ jQuery(document).ready(function($) {
         $(this).parent().toggleClass('active');
         $(this).siblings('.sub-menu').first().slideToggle(300);
     });
-    if ($('.counter').length) {
-        let options = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.5 // Trigger when 50% of the element is visible
-        };
 
-        // Create a new observer
-        let observer = new IntersectionObserver(function (entries, observer) {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    let $this = $(entry.target);
-                    var countTo = $this.attr("data-countto");
-                    var countDuration = parseInt($this.attr("data-duration"));
-
-                    // Trigger counter animation
-                    $({ counter: $this.find('span').text() }).animate({
-                        counter: countTo
-                    }, {
-                        duration: countDuration,
-                        easing: "linear",
-                        step: function () {
-                            $this.find('span').text(Math.floor(this.counter));
-                        },
-                        complete: function () {
-                            $this.find('span').text(this.counter);
-                        }
-                    });
-
-                    // Stop observing once the animation is triggered
-                    observer.unobserve(entry.target);
-                }
-            });
-        }, options);
-
-        // Target each element with the class .counter
-        $('.counter').each(function () {
-            observer.observe(this);
-        });
-    }
     $(".collapse-item .collapse-title").click(function () {
         if ($(this).closest(".collapse-item").hasClass("is-open")) {
            $(this).closest(".collapse-item").stop(true,true).removeClass("is-open");
@@ -94,118 +50,4 @@ jQuery(document).ready(function($) {
         }
         return false;
     });
-});
-if ($('.auto-width-slider').length) {
-    var autoWidthSplide = new Splide( '.auto-width-slider', {
-        type: 'loop',
-        height: '400px',
-        autoWidth: true,
-        arrows: false,
-        gap: '20px',
-        classes: {
-            pagination: 'splide__pagination icw-slide-dots',
-        },
-        breakpoints: {
-            992: {               
-                height: '300px',
-            },
-        }
-    });
-
-    autoWidthSplide.mount();
-}
-if ($('.single-slider').length) {
-    var singleSplide = new Splide( '.single-slider', {
-        type: 'slide',
-        arrows: false,
-        classes: {
-            pagination: 'splide__pagination icw-slide-dots',
-        }, 
-        breakpoints: {
-            992: {               
-                perPage: 1.5,
-                gap: '20px',
-            },
-            767: {               
-                perPage: 1,
-                gap: '20px',
-            },
-        }
-    });
-    singleSplide.mount();
-}
-if ($('.post-slider').length) {
-    var postSplide = new Splide( '.post-slider', {
-        type: 'slide',
-        perPage: 4,
-        perMove: 1,
-        gap: '30px',
-        arrows: true,
-        classes: {
-            pagination: 'splide__pagination icw-slide-dots is-light',
-        }, 
-        breakpoints: {
-            1200: {               
-                perPage: 3,
-                gap: '20px',
-            },
-            992: {               
-                perPage: 2,
-                gap: '20px',
-            },
-            767: {               
-                perPage: 1.2,
-                gap: '20px',
-            },
-        }
-    });
-    postSplide.mount();
-}
-if ($('.card-img-slider').length) {
-    var cardImgSplide = new Splide( '.card-img-slider', {
-        type: 'slide',
-        arrows: true,
-        classes: {
-            pagination: 'splide__pagination icw-slide-dots',
-        }, 
-    });
-    cardImgSplide.mount();
-}
-if ($('.feedback-slider').length) {
-    var cardImgSplide = new Splide( '.feedback-slider', {
-        type: 'slide',
-        perPage: 2,
-        perMove: 1,
-        arrows: false,
-        gap: '20px',
-        classes: {
-            pagination: 'splide__pagination icw-slide-dots',
-        }, 
-        breakpoints: {
-            767: {               
-                perPage: 1.5,
-                gap: '15px',
-            },
-            575: {               
-                perPage: 1,
-                gap: '15px',
-            },
-        }
-    });
-    cardImgSplide.mount();
-}
-if ($('.gallery-slider').length) {
-    var gallerySplide = new Splide( '.gallery-slider', {
-        type: 'loop',
-        arrows: false,
-        autoWidth: true,
-        autoplay: true,
-        gap: '30px',
-        pagination: false,
-        height: '300px',        
-    });
-    gallerySplide.mount();
-}
-var glightbox = GLightbox({
-    selector: ".glightbox",
 });
