2a61d2feac666419b3b0c86350dd8dc4db6f54ea
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2024-10-02 16:41:44 +0530

    Update Header View in Typeform HTML (PC)

--- typeform-html/html/assets/js/main.js
+++ typeform-html/html/assets/js/main.js
@@ -19,52 +19,51 @@ jQuery(document).ready(function($) {
         } else {
             $('.main-header').removeClass('fixed-header');
         }
-
         $(window).scroll(function () {
-            if (jQuery(this).scrollTop() > 150) {
+            if ($(this).scrollTop() > 150) {
                 $('.main-header').addClass('fixed-header');
-                if( $('.main-header').hasClass('fixed-header')) {
-                    $('.megamenu-overlay').height('88');
-                } else {
-                    $('.megamenu-overlay').height('0');
+                
+                if ($('.main-header').hasClass('fixed-header')) {
+                    // Check if the overlay has 'is-active' class
+                    if ($('.megamenu-overlay').hasClass('is-active')) {
+                        // Set height based on overlay_height (submenu height + header height)
+                        $('.megamenu-overlay').height(overlay_height + $('.main-header').height());
+                    } else {
+                        // Set height to 88 if overlay is not active
+                        $('.megamenu-overlay').height(88);
+                    }
                 }
             } else {
+                // Remove 'fixed-header' class and set overlay height to 0
                 $('.main-header').removeClass('fixed-header');
+                if ($('.megamenu-overlay').hasClass('is-active')) {
+                    $('.megamenu-overlay').height(overlay_height + $('.main-header').height());
+                } else {
+                    $('.megamenu-overlay').height(0);
+                }
             }
         });
         $(window).on('resize', function () {
             if ($(window).width() > 992) {
                 $('.menu-item-has-children').hover(
-                    function(){               
-                        var overlay_height = $(this).find('.sub-menu').outerHeight();
-                        $(this).parents('.main-header').find('.megamenu-overlay').addClass('is-active');
-                        $('.megamenu-overlay').height( overlay_height + $('.main-header').height());
+                    function() {
+                        overlay_height = $(this).find('.sub-menu').outerHeight()
+                        updateMegaMenuOverlay(this, true);
                     },
-                    function () {
-                        $(this).parents('.main-header').find('.megamenu-overlay').removeClass('is-active');
-                        if( $(this).parents('.main-header').hasClass('fixed-header')) {
-                            $('.megamenu-overlay').height('88');
-                        } else {
-                            $('.megamenu-overlay').height('0');
-                        }
+                    function() {
+                        updateMegaMenuOverlay(this, false);
                     }
                 );
             }
         });
         if ($(window).width() > 992) {
             $('.menu-item-has-children').hover(
-                function(){               
-                    var overlay_height = $(this).find('.sub-menu').outerHeight();
-                    $(this).parents('.main-header').find('.megamenu-overlay').addClass('is-active');
-                    $('.megamenu-overlay').height( overlay_height + $('.main-header').height());
+                function() {
+                    overlay_height = $(this).find('.sub-menu').outerHeight()
+                    updateMegaMenuOverlay(this, true);
                 },
-                function () {
-                    $(this).parents('.main-header').find('.megamenu-overlay').removeClass('is-active');
-                    if($('.main-header').hasClass('fixed-header')) {
-                        $('.megamenu-overlay').height('88');
-                    } else {
-                        $('.megamenu-overlay').height('0');
-                    }
+                function() {
+                    updateMegaMenuOverlay(this, false);
                 }
             );
         }
@@ -79,3 +78,20 @@ jQuery(document).ready(function($) {
         $(this).siblings('.sub-menu').first().slideToggle(300);
     });
 });
+
+var overlay_height = 0;
+function updateMegaMenuOverlay(element, isHover) {
+    var overlay = $(element).parents('.main-header').find('.megamenu-overlay');
+    
+    if (isHover) {
+        overlay.addClass('is-active');
+        overlay.height(overlay_height + $('.main-header').height());
+    } else {
+        overlay.removeClass('is-active');
+        if ($(element).parents('.main-header').hasClass('fixed-header')) {
+            overlay.height(88);
+        } else {
+            overlay.height(0);
+        }
+    }
+}
