202cfd58650e71e998a4b3344acbfdf1c82ab073
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2024-10-05 16:58:39 +0530

    Update New HTML Start for budownoble (PC)

---
+++ budownoble-html/html/assets/js/main.js
@@ -0,0 +1,40 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function($) {
+    if ($('[data-bs-toggle=tooltip]').length) {
+        $('body').tooltip({ selector: '[data-bs-toggle=tooltip]' });
+    }
+
+    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
+        $('.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+        e.preventDefault();
+    });
+
+    if ($('.main-header').length) {
+        if (jQuery(this).scrollTop() > 100) {
+            $('.main-header').addClass('fixed-header');
+        } else {
+            $('.main-header').removeClass('fixed-header');
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 100) {
+                $('.main-header').addClass('fixed-header');
+            } else {
+                $('.main-header').removeClass('fixed-header');
+            }
+        });
+    }
+    if ($('li.menu-item-has-children').length) {
+        $('li.menu-item-has-children > a').after('<i class="arrow"></i>');
+    }
+    $('li.menu-item-has-children > .arrow').on('click',function(event){
+        event.preventDefault();
+        $(this).toggleClass('is-active');
+        $(this).parent().toggleClass('active');
+        $(this).siblings('.sub-menu').first().slideToggle(300);
+    });
+});
