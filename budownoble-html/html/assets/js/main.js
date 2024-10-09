308172f17aed0a21bc1d42a34dd1bb6f425605c3
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2024-10-09 19:04:46 +0530

    Update Header in home for budownoble (PC)

--- budownoble-html/html/assets/js/main.js
+++ budownoble-html/html/assets/js/main.js
@@ -8,25 +8,65 @@ jQuery(document).ready(function($) {
         $('body').tooltip({ selector: '[data-bs-toggle=tooltip]' });
     }
 
-    $('.toggle-sidebar,.bg-overly').on('click', function (e) {
-        $('.toggle-sidebar,body,.main-header').toggleClass('is-visible');
+    $('.navbar-toggler').on('click', function (e) {
+        $('.toggle-sidebar, .megamenu-overlay, .main-header,.menu-list').toggleClass('is-visible');
         e.preventDefault();
     });
 
     if ($('.main-header').length) {
-        if (jQuery(this).scrollTop() > 100) {
+        if (jQuery(this).scrollTop() > 150) {
             $('.main-header').addClass('fixed-header');
         } else {
             $('.main-header').removeClass('fixed-header');
         }
-
         $(window).scroll(function () {
-            if (jQuery(this).scrollTop() > 100) {
+            if ($(this).scrollTop() > 150) {
                 $('.main-header').addClass('fixed-header');
+                
+                if ($('.main-header').hasClass('fixed-header')) {
+                    // Check if the overlay has 'is-active' class
+                    if ($('.megamenu-overlay').hasClass('is-active')) {
+                        // Set height based on overlay_height (submenu height + header height)
+                        $('.megamenu-overlay').height(overlay_height + $('.main-header').height());
+                    } else {
+                        // Set height to 88 if overlay is not active
+                        $('.megamenu-overlay').height(180);
+                    }
+                }
             } else {
+                // Remove 'fixed-header' class and set overlay height to 0
                 $('.main-header').removeClass('fixed-header');
+                if ($('.megamenu-overlay').hasClass('is-active')) {
+                    $('.megamenu-overlay').height(overlay_height + $('.main-header').height());
+                } else {
+                    $('.megamenu-overlay').height(0);
+                }
             }
         });
+        $(window).on('resize', function () {
+            if ($(window).width() > 992) {
+                $('.mega-menu').hover(
+                    function() {
+                        overlay_height = $(this).find('.sub-menu').outerHeight()
+                        updateMegaMenuOverlay(this, true);
+                    },
+                    function() {
+                        updateMegaMenuOverlay(this, false);
+                    }
+                );
+            }
+        });
+        if ($(window).width() > 992) {
+            $('.mega-menu').hover(
+                function() {
+                    overlay_height = $(this).find('.sub-menu').outerHeight()
+                    updateMegaMenuOverlay(this, true);
+                },
+                function() {
+                    updateMegaMenuOverlay(this, false);
+                }
+            );
+        }
     }
     if ($('li.menu-item-has-children').length) {
         $('li.menu-item-has-children > a').after('<i class="arrow"></i>');
@@ -38,3 +78,19 @@ jQuery(document).ready(function($) {
         $(this).siblings('.sub-menu').first().slideToggle(300);
     });
 });
+var overlay_height = 0;
+function updateMegaMenuOverlay(element, isHover) {
+    var overlay = $(element).parents('.main-header').find('.megamenu-overlay');
+    
+    if (isHover) {
+        overlay.addClass('is-active');
+        overlay.height(overlay_height + $('.main-header').height());
+    } else {
+        overlay.removeClass('is-active');
+        if ($(element).parents('.main-header').hasClass('fixed-header')) {
+            overlay.height(180);
+        } else {
+            overlay.height(0);
+        }
+    }
+}
