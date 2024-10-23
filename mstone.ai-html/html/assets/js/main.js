d423d3bf066d5cd0f6e77931216705c3ed000d63
InCreativeWeb DEMO <82993375+icwwpdemo@users.noreply.github.com>
2024-10-24 00:09:24 +0530

    Update Home Page in mstone site (PC)

---
+++ mstone.ai-html/html/assets/js/main.js
@@ -0,0 +1,60 @@
+/*-----------------------------------------------------------------------------------*/
+/* MAIN
+/*-----------------------------------------------------------------------------------*/
+var $ = jQuery.noConflict();
+
+jQuery(document).ready(function($) {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+    $('.navbar-toggler').on('click', function (e) {
+        $('.navbar-toggler,body,.main-header').toggleClass('is-visible');
+        e.preventDefault();
+    });
+
+    if ($('.main-header').length) {
+        if (jQuery(this).scrollTop() > 100) {
+            $(".main-header").addClass("fixed-header");
+        } else {
+            $(".main-header").removeClass("fixed-header");
+        }
+
+        $(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 100) {
+                $(".main-header").addClass("fixed-header");
+            } else {
+                $(".main-header").removeClass("fixed-header");
+            }
+        });
+    }
+
+    if ($('li.menu-item-has-children').length) {
+        $("li.menu-item-has-children > a").after('<i class="arrow"></i>');
+    }
+    $('li.menu-item-has-children .arrow').on('click',function(event){
+        event.preventDefault();
+        $(this).toggleClass('is-active');
+        $(this).parent().find('.sub-menu').first().toggle(300);
+    });
+    if ($('.tab-block').length) {
+        $('.nav-tabs .tab-link').on('click', function(){
+            // get the data attribute
+            var tab_id = $(this).attr('data-tab');
+            // remove the default classes
+            $('.nav-tabs .tab-link').removeClass('active');
+            $('.tab-pane').hide();
+            // add new classes on mouse click
+            $(this).addClass('active');
+            $('#'+tab_id).fadeIn();
+        });
+    }
+});
+if ($('.hero-splide-slider').length) {
+    var heroSplide = new Splide('.hero-splide-slider', {
+        type: 'slide',
+        arrows: false,
+        classes: {
+            pagination: 'splide__pagination icw-pagination',
+        },
+    });
+    heroSplide.mount();
+}
